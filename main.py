b767176c167a1e054859ba88e8a755fcba7e55ad
MatthewCaseres <matthewcaseres@outlook.com>
2023-04-24 22:43:24 -0500

    try to improve render time

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import pandas as pd
 import chainladder as cl
 
 st.markdown("# Hosting actuarial dashboards in Streamlit")
-st.markdown("Here is a big rectangle of data. We want to get triangles from this rectangle.")
-
+st.markdown("Here is the top 10 rows from a big rectangle of data. We want to get triangles from this rectangle.")
+n=10
 @st.cache_data(ttl=3600*10)
 def get_data():
     return pd.read_csv(
         "https://raw.githubusercontent.com/casact/chainladder-python/master/chainladder/utils/data/clrd.csv"
     )
-clrd_df = get_data()
+clrd_df = get_data().head(10)
 clrd_df
 st.markdown("""Casualty actuaries create reports with \"loss triangles\". 
 For the triangles we use the [chainladder-python](https://chainladder-python.readthedocs.io/en/latest/intro.html) package. This package is published by the Casualty Actuarial Society and is
